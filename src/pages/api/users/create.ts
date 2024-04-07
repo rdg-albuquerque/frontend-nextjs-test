47b5bac13c7f46c49d25642f5714674d46fec187
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2024-04-07 01:56:04 -0300

    Solving form

--- src/pages/api/users/create.ts
+++ src/pages/api/users/create.ts
@@ -13,9 +13,71 @@
 import { NextApiRequest, NextApiResponse } from 'next/types';
 
 import { IUser, IUserCreate } from '@/types/user.d';
+import { ApiMethod } from '@/decorators/method';
+
+const validateSchema = (data: IUserCreate) => {
+	// It would be nice to use zod or yup for this
+	let response = {
+		valid: false,
+		error: '',
+		data
+	};
+
+	if (!data.name) {
+		response.error = "name is required";
+		return response;
+	}
+
+	if (typeof data.name !== 'string') {
+		response.error = "name must be string";
+		return response;
+	}
+
+	if (!data.email) {
+		response.error = "email is required";
+		return response;
+	}
+
+	if (typeof data.email !== 'string') {
+		response.error = "email must be string";
+		return response;
+	}
+
+	if (typeof data.email !== 'string') {
+		response.error = "email must be string";
+		return response;
+	}
+
+	if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(data.email)) {
+		response.error = "email format is invalid";
+		return response;
+	}
+
+	response.valid = true;
+	return response;
+}
 
 const users: IUser[] = [];
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-	return res.status(400).json(undefined);
-};
+export default ApiMethod('POST')(async (req: NextApiRequest, res: NextApiResponse) => {
+
+	const userSchema = validateSchema(req.body);
+
+	if (!userSchema.valid) {
+		return res.status(400).json({message: userSchema.error});
+	}
+
+	if (users.find(user => user.email === userSchema.data.email)) {
+		return res.status(400).json({
+			code: 4,
+			message: 'Email já está em uso'
+		});
+	}
+
+	users.push({
+		id: users.length + 1,
+		...userSchema.data,
+	});
+
+	return res.status(201).json({message: 'Usuário criado!'});
+})
--- src/pages/formulario.tsx
+++ src/pages/formulario.tsx
@@ -10,20 +10,66 @@
  */
 
 import styles from '@/styles/formulario.module.css';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type FormData = {
+	name: string
+	email: string
+}
 
 export default function Form() {
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm<FormData>()
 
-		console.log('submit');
+	const onSubmit: SubmitHandler<FormData> = async (formData) => {
+		try {
+			const response = await fetch('/api/users/create', {
+				method: 'POST',
+				body: JSON.stringify(formData),
+				headers: {
+					'Content-Type': 'application/json',
+				}
+			});
+	
+			if (!response.ok) {
+				const error = await response.json();
+				if (error.code === 4) {
+					return alert(error.message)
+				}
+				throw new Error('Houve um erro ao criar a conta')
+			}
+			reset();
+			alert('Conta criada com sucesso')
+		} catch (error) {
+			console.error(error)
+		}
 	}
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
-				<form onSubmit={handleSubmit}>
-					<input type="text" placeholder="Name" />
-					<input type="email" placeholder="E-mail" />
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input {...register('name', {required: 'O campo nome é obrigatório'})} type="text" placeholder="Name" />
+					{errors.name && (
+						<span role="alert">{errors.name.message}</span>
+					)}
+					<input {...register('email', {
+						required: "O campo e-mail é obrigatório",
+						pattern: {
+							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+							message: "E-mail inválido"
+						  }
+					})}
+						type='email' 
+					 	placeholder="E-mail" 
+					/>
+					{errors.email && (
+						<span role="alert">{errors.email.message}</span>
+					)}
 
 					<button type="submit" data-type="confirm">
 						Enviar
